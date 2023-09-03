66d3b24d10a9abefc2a04657d64d57eaef7c6cd7
二货机器人 <smith3816@gmail.com>
2023-09-03 14:14:32 +0800

    docs: update dumi config

--- .dumirc.ts
+++ .dumirc.ts
@@ -8,8 +8,8 @@ const isProdSite =
 
 export default defineConfig({
   alias: {
-    'antd-geek-theme-sample$': path.resolve('src'),
-    'antd-geek-theme-sample/es': path.resolve('src'),
+    [`${name}$`]: path.resolve('src'),
+    [`${name}/es`]: path.resolve('src'),
   },
   mfsu: false,
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
